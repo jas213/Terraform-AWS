9e95ab0ee8b69d6f92234226f2036828d3b68a69
Jasmine <38283275+jas213@users.noreply.github.com>
2024-11-22 19:31:26 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,5 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "eu-west-1"
-  profile = "terraform"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }

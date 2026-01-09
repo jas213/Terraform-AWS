0a52e85ea26cdec1c69b2e2c8414abe78ac2bd31
Jasmine <38283275+jas213@users.noreply.github.com>
2026-01-09 18:31:39 +0530

    Add files via upload

--- modules/subnet/outputs.tf
+++ modules/subnet/outputs.tf
@@ -2,3 +2,7 @@ output "public_subnet_ids" {
   description = "Public Subnet IDs"
   value = aws_subnet.public_subnet[*].id
 }
+output "private_subnet_ids" {
+  description = "Private Subnet IDs"
+  value = aws_subnet.private_subnet[*].id
+}

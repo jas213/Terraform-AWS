26de0cab031eec19529744dd056fed079c0a5bc6
Jasmine <38283275+jas213@users.noreply.github.com>
2026-01-09 16:49:13 +0530

    Add files via upload

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,1 +1,31 @@
+#Calling Modules
 
+#Create VPC Network
+module "vpc" {
+  source = "../../modules/vpc"
+}
+
+#Create Subnetwork
+module "subnet" {
+  source = "../../modules/subnet"
+  vpc_id = module.vpc.vpc_id
+}
+
+#Create Internet Gateway
+module "ig" {
+  source = "../../modules/internet_gateway"
+  vpc_id = module.vpc.vpc_id
+}
+#Create Elastic IP
+#Note: EIP may require IGW to exist prior to association. Use depends_on to set an explicit dependency on the IGW.
+module "elastic_ip" {
+  source = "../../modules/elastic_ip"
+  subnet_ids = module.subnet.public_subnet_ids
+}
+
+#Create NAT Gateway
+module "ng" {
+  source = "../../modules/nat_gateway"
+  subnet_ids = module.subnet.public_subnet_ids
+  allocation_ids = module.elastic_ip.allocation_ids
+}
---
+++ environments/dev/variables.tf
@@ -0,0 +1,8 @@
+variable "region" {
+  type = string
+  default = "us-west-1"
+}
+variable "aws_profile" {
+  type = string
+  default = "Jasmine"
+}
